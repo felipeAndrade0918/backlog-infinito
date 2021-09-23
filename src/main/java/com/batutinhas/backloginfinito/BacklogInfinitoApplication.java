f1f2af8b83ba775e74a202a7784a26dc2256e10a
Felipe Andrade <felipe.andrade0918@gmail.com>
2021-09-23 18:13:35 -0300

    Primeiro commit
    O primeiro de muitos!

---
+++ src/main/java/com/batutinhas/backloginfinito/BacklogInfinitoApplication.java
@@ -0,0 +1,13 @@
+package com.batutinhas.backloginfinito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogInfinitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogInfinitoApplication.class, args);
+	}
+
+}
