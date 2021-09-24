a5df9148d95d9d67c324f6ffc78731081b4af7ce
Felipe Andrade <felipe.andrade0918@gmail.com>
2021-09-24 20:47:08 -0300

    Adiciona Spock

---
+++ src/test/groovy/com/batutinhas/backloginfinito/BacklogInfinitoApplicationSpec.groovy
@@ -0,0 +1,13 @@
+package com.batutinhas.backloginfinito
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class BacklogInfinitoApplicationSpec extends Specification {
+
+    @Test
+    void contextLoads() {
+    }
+}
